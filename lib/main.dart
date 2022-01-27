3fb4651941df70319e96ddbea42b80deea95c171
pratham <pra102030@gmail.com>
2022-01-28 03:01:00 +0530

    Added photo to login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first/pages/home_page.dart';
 import 'package:first/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.indigo),
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
 
       darkTheme: ThemeData(
         brightness: Brightness.dark,
@@ -23,6 +27,7 @@ class MyApp extends StatelessWidget {
       routes: {
         "/": (context) => LoginPage(),
         "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,13 +3,28 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(fontSize: 40, color: Colors.deepOrangeAccent),
-        ),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login01.png",
+          ),
+          Text(
+            "Login",
+            style: TextStyle(
+              fontSize: 22,
+            ),
+          )
+        ],
       ),
+
+      // child: Center(
+      //   child: Text(
+      //     "Login Page",
+      //     style: TextStyle(fontSize: 40, color: Colors.deepOrangeAccent),
+      //   ),
+      // ),
     );
   }
 }
